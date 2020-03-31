5fd41f6509c142824d0e6fcec06c6179118dd133
yashraj021 <yash.raj021@gmail.com>
2020-04-01 04:13:15 +0530

    Added CORS.

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
+
 app.use('/api/places', PlacesRoutes);
 
 app.use('/api/user', UsersRoutes);
