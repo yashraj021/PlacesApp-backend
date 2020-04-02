df75c1b079a3e88569b0d61c4b3ca9284f4ccd20
yashraj021 <yash.raj021@gmail.com>
2020-04-03 02:38:49 +0530

    ADded port change

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 })
 
 mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => 
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
 ).catch(err => 
     console.log(err)
 );
